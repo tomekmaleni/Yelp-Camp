2d608e59fc38434287f83fb1350eed5743805b95
Tomislav Matokovic <tomekmaleni@gmail.com>
2021-07-07 14:31:13 +0200

    adding image, description and price to seeds

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = random1000 / 50 + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.Quae quaerat quasi maxime sapiente temporibus quibusdam libero vero cum doloremque aliquam aliquid rem laborum nihil adipisci, voluptates, praesentium quos laudantium sint?",
+            price
         })
         await camp.save();
     }
